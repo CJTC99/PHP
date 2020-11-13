29b3605b39b3afcd63a41e7a59ce571b1c684b99
CJTC99 <cit2_18@alu.uabcs.mx>
2020-11-12 22:09:29 -0700

    subiendo mi primer php work

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+	$name ="Cinthya";
+	$lastname ="Turrubiates";
+	$fullname =$name."".$lastname;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		PHP
+	</title>
+</head>
+<body>
+	<h1>
+		Me llamo <?= $fullname; ?>  
+	</h1>
+</body>
+</html>
---
+++ info.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+	<?php  if(true): ?>
+		<p>
+			Al roberto le gusta el tomate.
+		</p>
+	<?php else: ?>
+		<p>
+			Al alexis le gusta LA cinthya uwu
+		</p>
+	<?php endif?>
+
+</body>
+</html>
